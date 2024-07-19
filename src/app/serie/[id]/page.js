c1b9e809cbd2cb8de18c5f57acadbb03c4437409
mariafer057 <mariafernandaalves057@gmail.com>
2024-07-18 20:25:13 -0400

    18/07
    (agora sim, estava implementando outras coisas sobre os dados da API) Implementando a rota dinamica - Requisito #3

---
+++ src/app/serie/[id]/page.js
@@ -0,0 +1,20 @@
+import Image from "next/image"
+
+
+export default async function Produto({params}){
+    const response = await fetch("http://localhost:3000/api/" + params.id)
+    const data = await response.json();
+    return(
+        <div key={data.id}>
+          <h2 >Título: {data.titulo}</h2>
+          <h2>Diretor : {data.diretor}</h2>
+          <h2>Ano de publicacao : {data.anoPublicacao}</h2>
+          <h2>Genero: {data.genero}</h2>
+          <h2>Temporadas: {data.temporadas}</h2>
+          <Image 
+          width= {150}
+          height={150}
+          src={data.image}/>
+        </div>
+  )
+}  
