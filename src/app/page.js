0c32bf52858840b5f6f1af85255c8f810663bae0
mariafer057 <mariafernandaalves057@gmail.com>
2024-07-18 20:48:55 -0400

    18/07
    terminei de implementar o useState e useEffect - Requisito #3

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,42 @@
+'use client'
 import Image from "next/image";
 import "./styles.css";
 import Link from "next/link";
+import Header from "./Componetes/Header";
+import Carregar from "./Componetes/Carregar";
+import Footer from "./Componetes/Footer";
+import { useEffect, useState } from "react";
+import ErrorGetData from "./Componetes/ErrorGetData";
+
 
 export default function Home() {
 
-  return (
-    <main>
-     
+    const [listaSeries, setListaSeries] = useState([]);
+    const [listaComplete, setListaComplete] = useState([]);
+    const [errorFetch, setErrorFetch] = useState(false);
+
+    useEffect(()=>{
+        const getSeries = async()=>{ 
+        try{
+          const response = await fetch("http://localhost:3000/api");
+          const data = await response.json();
+          setListaSeries(data);
+        }catch{
+            setErrorFetch(true);
+        };
+      };
+
+      getSeries();
+    }, []);
+
+    if(errorFetch == true){
+      return <ErrorGetData/>
+    }
+
+    if (listaSeries[0] == null){
+      return <center><Carregar/></center>
+    }
+
     </main>
   );
 }
