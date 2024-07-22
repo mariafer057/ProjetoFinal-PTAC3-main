931ab1e547e0f961e841ccaa4ec7c2dbe55c55f3
mariafer057 <mariafernandaalves057@gmail.com>
2024-07-22 17:15:59 -0400

    22/07
    * implementamos o search para realizar as pesquisas dos itens.
    
    * implementamos a constante para buscar as series por ordem alfabética de Az e de Za
    
    *implementamos a constante para buscar a quantidade de temporadas do maior numero para o menor numero (utilizamos o código do shop-main).

--- src/app/page.js
+++ src/app/page.js
@@ -13,6 +13,7 @@ export default function Home() {
 
     const [listaSeries, setListaSeries] = useState([]);
     const [listaComplete, setListaComplete] = useState([]);
+    const [search, setSearch] = useState("");
     const [errorFetch, setErrorFetch] = useState(false);
 
     useEffect(()=>{
@@ -28,6 +29,50 @@ export default function Home() {
 
       getSeries();
     }, []);
+      //ordem de Az
+      const orderAz = () => {
+        const newList = [...listaSeries].sort((a, b) => 
+        a.title.localeCompare(b.title)
+        );
+        setListaSeries(newList);
+      }
+
+      //ordem de Za
+      const orderZa = () =>{
+        const newList = [...listaSeries].sort((a, b)=>
+          b.title.localeCompare(a.title)
+        );
+        setListaSeries(newList);
+      }
+  
+      //temporadas
+      const tempMaior = () =>{
+        let newList = [...listaSeries].sort((a, b)=> 
+          a.temporadas - b.temporadas
+        );
+        setListaSeries(newList); 
+      }
+
+      const tempMenor = () =>{
+        let newList = [...listaSeries].sort((a, b)=>
+          a.temporadas - b.temporadas
+        );
+        newList = newList.reverse(); //o reverse serve para inverter oq será feito, sem mudar muito o codigo
+        setListaSeries(newList); //atualiza o estado listProdutos com a nova lista ordenada
+      }
+
+      //filtrar o listaSeries
+      const searchText = (text) => {
+        setSearch(text);
+        if(text.trim() == ""){
+          setListaSeries(listaComplete);
+          return
+        }
+        const newList = listaSeries.filter((series) =>
+        series.title.toUpperCase().trim().includes(search.toUpperCase().trim()));
+        setListaSeries(newList);
+      }
+
 
     if(errorFetch == true){
       return <ErrorGetData/>
@@ -36,7 +81,42 @@ export default function Home() {
     if (listaSeries[0] == null){
       return <center><Carregar/></center>
     }
+    return(
+      <>
+       <input type="text" value={search} placeholder="Pesquise a série" onChange={(event) => searchText( event.target.value)}/>
+    <main className={styles.main}>
+      <div>
+    <button className={styles.botao} onClick={orderAz}>Az</button>
+    </div>
 
+    <div>
+    <button className={styles.botao} onClick={orderZa}>Za</button>
+    </div>
+
+    <div>
+    <button className={styles.botao} onClick={tempMaior}>-</button>
+    </div>
+
+    <div>
+    <button className={styles.botao} onClick={tempMenor}>+</button>
+    </div>
+
+      {listaSeries.map((series)=> //o map vai mapear cada produto de uma forma individual
+            <div className={styles.card} key={series.id}>
+              <h2 className={styles.titulo}>Título: {series.title}</h2>
+              <h2>Diretor: {series.diretor}</h2>
+              <h2 className={styles.desc}>Ano de Publicação: {series.anoPublicacao}</h2>
+              <h2>Gênero: {series.genero}</h2>
+              <h2>Temporadas: {series.temporadas}</h2>
+              <Image 
+              width= {150}
+              height={150}
+              src={series.image}/>
+              <Link href={"/series/" + series.id}><p>Ver mais</p></Link>
+
+            </div>
+      )}
     </main>
+    </>
   );
 }
