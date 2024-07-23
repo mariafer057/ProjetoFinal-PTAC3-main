450b928704b4c0e76c87b25d01bf13a7ef2af457
mariafer057 <mariafernandaalves057@gmail.com>
2024-07-22 20:16:59 -0400

    22/07
    realizando a correção dos dominios do servidor #SERVER

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,12 @@ const nextConfig = {
 
     images:{
         domains:["m.media-amazon.com",
-             "nflxso.net",
-             "glbimg.com",
-             "mydramalist.com",
-             "googleusercontent.com"
+             "occ-0-7515-185.1.nflxso.net",
+             "www.apple.com",
+             "s2-globo-play.glbimg.com",
+             "i.mydramalist.com",
+             "blogger.googleusercontent.com",
+             ""
         ]
     }
 };
